b7b573e5ffae0ac5d905566c33e9a84233aaa927
DESKTOP-T5FIHTK\User <yaojenliang@gmail.com>
2025-05-25 16:42:49 +0800

    feat：新增4個subnet、NAT與NAT所需的EIP

--- 03-examples/aws-vpc-example/main.tf
+++ 03-examples/aws-vpc-example/main.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.16"
+      version = "~> 5.0"
     }
   }
 
@@ -13,15 +13,59 @@ provider "aws" {
   region  = "us-west-1"
 }
 
-resource "aws_vpc" "kevin_main" {
-  cidr_block = "192.168.0.0/16"
-
+# vpc
+resource "aws_vpc" "terraform_vpc" {
+  cidr_block = "10.0.0.0/16"
+  enable_dns_support = true
+  enable_dns_hostnames = true
   tags = {
-    Name = "kevin"
+    Name = "terraform_vpc"
   }
 }
 
-resource "aws_subnet" "public_subnet" {
-    cidr_block = "192.168.1.0/24"
-    vpc_id = aws_vpc.kevin_main.id
+#subnet
+resource "aws_subnet" "public_subnet_1" {
+    cidr_block = "10.0.0.0/24"
+    vpc_id = aws_vpc.terraform_vpc.id
+}
+
+resource "aws_subnet" "public_subnet_2" {
+    cidr_block = "10.0.1.0/24"
+    vpc_id = aws_vpc.terraform_vpc.id
+}
+
+resource "aws_subnet" "private_subnet_1" {
+    cidr_block = "10.0.2.0/24"
+    vpc_id = aws_vpc.terraform_vpc.id
+}
+
+resource "aws_subnet" "private_subnet_2" {
+    cidr_block = "10.0.3.0/24"
+    vpc_id = aws_vpc.terraform_vpc.id
+}
+
+
+# IGW
+resource "aws_internet_gateway" "igw" {
+    vpc_id = aws_vpc.terraform_vpc.id
+
+}
+
+#EIP
+resource "aws_eip" "nat_eip" {
+  domain   = "vpc"
+  tags  = {
+    Name =  "nat-eip"
+  }
+}
+
+# NAT
+resource "aws_nat_gateway" "nat" {
+  allocation_id = aws_eip.nat_eip.id
+  subnet_id     = aws_subnet.public_subnet_1.id
+
+  tags = {
+    Name = "nat-gw"
+  }
+  depends_on = [aws_internet_gateway.igw]
 }
