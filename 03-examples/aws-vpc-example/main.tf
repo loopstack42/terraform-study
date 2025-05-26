380ca2cc707bc632f23e674cd303ea40ce7a43bf
DESKTOP-T5FIHTK\User <yaojenliang@gmail.com>
2025-05-26 23:04:57 +0800

    feat：新增public route table 跟route table association next：新增private route table跟route table association

--- 03-examples/aws-vpc-example/main.tf
+++ 03-examples/aws-vpc-example/main.tf
@@ -69,3 +69,28 @@ resource "aws_nat_gateway" "nat" {
   }
   depends_on = [aws_internet_gateway.igw]
 }
+
+#Route Table
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.terraform_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public route table"
+  }
+}
+
+# Route Table association
+resource "aws_route_table_association" "public_routeTable_association_1" {
+  subnet_id      = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
+resource "aws_route_table_association" "public_routeTable_association_2" {
+  subnet_id      = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_route_table.id
+}
